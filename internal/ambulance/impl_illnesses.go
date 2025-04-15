023cc9bad8474be96cbe33a5986f0bd01313b13c
LuborKoka <koka.lubor87@gmail.com>
2025-04-15 21:12:05 +0200

    patch sick leave end date

--- internal/ambulance/impl_illnesses.go
+++ internal/ambulance/impl_illnesses.go
@@ -206,6 +206,7 @@ func (i *implIlnessesAPI) DeleteIllness(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status":  "OK",
 		"message": "Illness deleted successfully",
+		"patient": patient,
 	})
 }
 
@@ -272,7 +273,104 @@ func (i *implIlnessesAPI) GetPatientIllnesses(c *gin.Context) {
 
 // UpdateSickLeaveEndDate implements IllnessesAPI.
 func (i *implIlnessesAPI) UpdateSickLeaveEndDate(c *gin.Context) {
-	c.AbortWithStatus(http.StatusNotImplemented)
+	value, exists := c.Get("db_service")
+	if !exists {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  "Internal Server Error",
+				"message": "db not found",
+				"error":   "db not found",
+			})
+		return
+	}
+
+	db, ok := value.(db_service.DbService[Patient])
+	if !ok {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  "Internal Server Error",
+				"message": "db context is not of required type",
+				"error":   "cannot cast db context to db_service.DbService",
+			})
+		return
+	}
+
+	patientID := c.Param("patientId")
+
+	patient, err := db.FindDocument(c, patientID)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			c.JSON(
+				http.StatusBadRequest,
+				gin.H{
+					"status":  "Bad Request",
+					"message": "Patient not found",
+					"error":   "Wrong patientId probably",
+				},
+			)
+		} else {
+			c.JSON(
+				http.StatusInternalServerError,
+				gin.H{
+					"status":  "Internal Server Error",
+					"message": "Failed to retrieve the patient",
+					"error":   err.Error(),
+				})
+		}
+		return
+	}
+
+	type UpdateIllnessRequest struct {
+		IllnessId string `json:"illness_id" binding:"required"`
+		SlUntil   string `json:"sl_until"`
+	}
+
+	var illnessRequest UpdateIllnessRequest
+	if err := c.ShouldBindJSON(&illnessRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	found := false
+	for idx := range patient.Illnesses {
+		if patient.Illnesses[idx].Id == illnessRequest.IllnessId {
+			patient.Illnesses[idx].SlUntil = illnessRequest.SlUntil
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "Bad Request",
+			"message": "Illness not found for this patient",
+			"error":   "Invalid illness_id",
+		})
+		return
+	}
+
+	err = db.UpdateDocument(c, patientID, patient)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  "Internal Server Error",
+				"message": "Failed to update patient's illness",
+				"error":   err.Error(),
+			},
+		)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "OK",
+		"message": "Illness updated successfully",
+		"patient": patient,
+	})
+
 }
 
 func IllnessesApi() IllnessesAPI {
